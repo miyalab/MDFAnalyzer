4c1382eb0503eb39ae3ee645baeb38b02063d6d9
k.miyauchi <60140464+miyalab@users.noreply.github.com>
2025-10-29 07:56:46 +0900

    Update ThreadPool.py

--- script/ThreadPool.py
+++ script/ThreadPool.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import threading
 from typing import List, Any, Tuple, Dict
 import copy
+import queue
 
 class ThreadPool:
     class Result:
@@ -31,7 +32,7 @@ class ThreadPool:
         self.__is_running             : bool = True
         self.__threads                : List[str] = []
         self.__call_wait_function_lock: threading.Lock = threading.Lock()
-        self.__wait_function_infos    : List[ThreadPool.Info] = []
+        self.__wait_function_infos    : queue.Queue = queue.Queue()
 
     def start(self):
         self.__is_running = True
@@ -49,11 +50,11 @@ class ThreadPool:
         return len(self.__threads)
     
     def getWaitThreadNum(self) -> int:
-        return len(self.__wait_function_infos)
+        return self.__wait_function_infos.qsize()
 
     def submit(self, target: object, *args, **kwargs) -> ThreadPool.Result:
         result = ThreadPool.Result()
-        self.__wait_function_infos.append(ThreadPool.Info(target, result, *args, **kwargs))
+        self.__wait_function_infos.put(ThreadPool.Info(target, result, *args, **kwargs))
         self.__callWaitFunction()
         return result
 
@@ -67,10 +68,18 @@ class ThreadPool:
     def __callWaitFunction(self):
         ### 待機中の関数実行
         with self.__call_wait_function_lock:
-            if self.isRunning() and len(self.__threads) < self.max_thread_num and len(self.__wait_function_infos) > 0:
-                thread = threading.Thread(target=self.__executeFunction, args=[self.__wait_function_infos.pop()])
+            if threading.main_thread().is_alive() and self.isRunning() and len(self.__threads) < self.max_thread_num and self.__wait_function_infos.qsize() > 0:
+                thread = threading.Thread(target=self.__executeFunction, args=[self.__wait_function_infos.get()])
                 self.__threads.append(thread)
                 thread.start()
 
 if __name__ == "__main__":
-    pass
+    def func():
+        print(threading.current_thread().name)
+
+    thread_pool = ThreadPool(5)
+    thread_pool.submit(func)
+    thread_pool.submit(func)
+    thread_pool.submit(func)
+    thread_pool.submit(func)
+    thread_pool.submit(func)
